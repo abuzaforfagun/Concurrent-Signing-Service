4178eda1b42e5eca389437abed8d51093101f5f0
Fagun <fagun@rushdigital.co.nz>
2024-04-22 22:10:15 +0600

    Fix encyption and decryption

--- src/ConcurrentSigning.Cryptography/SymmetricEncryption.cs
+++ src/ConcurrentSigning.Cryptography/SymmetricEncryption.cs
@@ -9,7 +9,7 @@ public static class SymmetricEncryption
     {
         using Aes aesAlg = Aes.Create();
         aesAlg.Key = Encoding.ASCII.GetBytes(key);
-        aesAlg.IV = new byte[16];
+        aesAlg.GenerateIV(); // Generate a random IV for each encryption
 
         ICryptoTransform encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
 
@@ -18,18 +18,29 @@ public static class SymmetricEncryption
         using StreamWriter swEncrypt = new StreamWriter(csEncrypt);
         swEncrypt.Write(plainText);
 
-        return msEncrypt.ToString();
+        swEncrypt.Flush();
+        csEncrypt.FlushFinalBlock();
+
+        byte[] ivAndEncryptedText = new byte[aesAlg.IV.Length + msEncrypt.Length];
+        Buffer.BlockCopy(aesAlg.IV, 0, ivAndEncryptedText, 0, aesAlg.IV.Length);
+        Buffer.BlockCopy(msEncrypt.ToArray(), 0, ivAndEncryptedText, aesAlg.IV.Length, (int)msEncrypt.Length);
+
+        return Convert.ToBase64String(ivAndEncryptedText);
     }
 
     public static string Decrypt(string cipherText, string key)
     {
+        byte[] ivAndEncryptedText = Convert.FromBase64String(cipherText);
+
         using Aes aesAlg = Aes.Create();
         aesAlg.Key = Encoding.ASCII.GetBytes(key);
-        aesAlg.IV = new byte[16];
+        byte[] iv = new byte[aesAlg.IV.Length];
+        Buffer.BlockCopy(ivAndEncryptedText, 0, iv, 0, aesAlg.IV.Length);
+        aesAlg.IV = iv;
 
         ICryptoTransform decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
 
-        using MemoryStream msDecrypt = new MemoryStream(Encoding.ASCII.GetBytes(cipherText));
+        using MemoryStream msDecrypt = new MemoryStream(ivAndEncryptedText, aesAlg.IV.Length, ivAndEncryptedText.Length - aesAlg.IV.Length);
         using CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read);
         using StreamReader srDecrypt = new StreamReader(csDecrypt);
         return srDecrypt.ReadToEnd();
